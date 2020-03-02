7a8a3d58b9df737697042279e4a57b2ecdc53c86
Akshay R. Kapadia <akshayrajankapadia@gmail.com>
2020-03-02 14:34:11 -0500

    Added example usage of code

--- web_scraper.py
+++ web_scraper.py
@@ -20,17 +20,18 @@ def getValue(raw_object, attr, htmlClass):
     value = raw_object.find(attr, class_=htmlClass)
     return value
 
-
-def getMovies(url):
-    soup = getSoup(url)
-    raw_movies = soup.find_all("tr", class_="css-o6sgwe")
-    movies = []
-    for raw_movie in raw_movies:
-        raw_title = getValue(raw_movie, "td", "css-1u7zfla e126mwsw1")
-        title = raw_title.text
-        link = "https://reelgood.com" + raw_title.a.attrs["href"]
-        values = getValues(raw_movie, "td", "css-1u11l3y")
-        releaseYear = values[0].text
-        rating = values[2].text
-        movies.append(Movie(title, releaseYear, rating, link))
-    return movies
+# Example Usage
+#
+# def getMovies(url):
+#     soup = getSoup(url)
+#     raw_objects = soup.find_all("tr", class_="css-o6sgwe")
+#     movies = []
+#     for raw_movie in raw_movies:
+#         raw_title = getValue(raw_movie, "td", "css-1u7zfla e126mwsw1")
+#         title = raw_title.text
+#         link = "https://reelgood.com" + raw_title.a.attrs["href"]
+#         values = getValues(raw_movie, "td", "css-1u11l3y")
+#         releaseYear = values[0].text
+#         rating = values[2].text
+#         movies.append(Movie(title, releaseYear, rating, link))
+#     return movies
