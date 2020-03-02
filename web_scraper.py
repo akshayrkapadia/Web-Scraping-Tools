24df32c67738e65c8a12190eb034ecda9b49eec2
Akshay R. Kapadia <akshayrajankapadia@gmail.com>
2020-03-02 14:32:36 -0500

    Added web scraping python functions

---
+++ web_scraper.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+from movie import Movie
+
+
+def getSoup(url):
+    result = requests.get(url)
+    source = result.text
+    soup = BeautifulSoup(source, 'html.parser')
+    return soup
+
+
+def getValues(raw_object, attr, htmlClass):
+    values = raw_object.find_all(attr, class_=htmlClass)
+    return values
+
+
+def getValue(raw_object, attr, htmlClass):
+    value = raw_object.find(attr, class_=htmlClass)
+    return value
+
+
+def getMovies(url):
+    soup = getSoup(url)
+    raw_movies = soup.find_all("tr", class_="css-o6sgwe")
+    movies = []
+    for raw_movie in raw_movies:
+        raw_title = getValue(raw_movie, "td", "css-1u7zfla e126mwsw1")
+        title = raw_title.text
+        link = "https://reelgood.com" + raw_title.a.attrs["href"]
+        values = getValues(raw_movie, "td", "css-1u11l3y")
+        releaseYear = values[0].text
+        rating = values[2].text
+        movies.append(Movie(title, releaseYear, rating, link))
+    return movies
